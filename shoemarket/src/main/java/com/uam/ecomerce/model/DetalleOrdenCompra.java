f5760470308a1d5bf0470d26d4619930d4fa11f0
Marcelo Hernández <mhernandezruiz007@gmail.com>
2022-10-23 12:34:42 -0600

    Implement Order Detail

--- shoemarket/src/main/java/com/uam/ecomerce/model/DetalleOrdenCompra.java
+++ shoemarket/src/main/java/com/uam/ecomerce/model/DetalleOrdenCompra.java
@@ -11,4 +11,20 @@ import java.util.UUID;
 @Data
 public class DetalleOrdenCompra {
 
+    @Id
+    @SequenceGenerator(name = "DetalleOrdenCompra_seq",
+            sequenceName = "DetalleOrdenCompra_seq",
+            allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "DetalleOrdenCompra_seq")
+    private UUID ID;
+    private Long ProductoID;
+    private int Cantidad;
+    private Double Precio;
+
+    @ManyToOne
+    @JoinColumn(name="ID_OrdenCompra")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private OrdenCompra OrdenCompra;
+
 }
